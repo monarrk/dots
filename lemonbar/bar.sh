f618a9cde2600ae6332f65ef2da41bfd51b4b7b2
Skye Bleed <monarrk@aaathats3as.com>
2019-11-24 11:34:37 -0500

    Cleaning

--- lemonbar/bar.sh
+++ lemonbar/bar.sh
@@ -9,24 +9,11 @@ Clock() {
 
 #Define the battery
 Battery() {
-        BATPERC=$(apm -l)
-	if [ $BATPERC -lt 15 ]; then
-		BATPERC="%{F#FF0000}$BATPERC"
-	fi
-
-	if [ $(apm -a) -eq 1 ]; then
-		COL="%{F#FFFF00}"
-	fi
-        echo "$COL$BATPERC%%%{F#FFFFFF}"
-}
-
-Mute() {
-	if [ $(mixerctl outputs.master.mute | cut -d"=" -f2)=="on" ]; then
-		echo " %{F#FF0000}[MUTE]%{F#FFFFFF}"
-	fi
+        BATPERC=$(acpi | cut -d"," -f2)
+        echo "$BATPERC"
 }
 
 while xset -q &> /dev/null; do
-	echo "%{r}<volume: $(volcli get)$(Mute)> <brightness: $(xbacklight -get)> <time: $(Clock)> <battery: $(Battery)>  "
+	echo "%{r}%{F#FFFFFF} <network: $(iwgetid -s wlan0)> <time: $(Clock)> <battery:$(Battery)>  "
 	sleep 1
 done
